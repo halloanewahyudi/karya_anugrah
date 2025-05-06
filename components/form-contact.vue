6d90c2fe222778fe59a90c4bfcd1115559b39097
halloanewahyudi <hallo.anewahyudi@gmail.con>
2025-05-06 23:23:06 +0700

    b

---
+++ components/form-contact.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="">
+     <form @submit.prevent="formSubmit" action="" class="flex flex-col gap-4">
+      <div class="flex flex-col gap-3">
+     
+        <input type="text" placeholder="Name" v-model="formData.name" class="input">
+      </div>
+      <div>
+      
+        <input type="email" placeholder="Email" v-model="formData.email"  class="input">
+      </div>
+      <div>
+        
+        <input type="text" placeholder="Subject" v-model="formData.subject"  class="input">
+      </div>
+      <div>
+    
+        <textarea name="" id="" cols="30" rows="4" v-model="formData.message" placeholder="Message" class="input"></textarea>
+      
+      </div>
+
+      <button class="btn max-w-max">Submit <Icon name="ri:send-plane-fill" /> </button>
+     </form>
+  </div>
+</template>
+
+<script lang="ts" setup>
+ const formData = ref({})
+ const formSubmit = () => {
+  console.log(formData);
+ }
+
+</script>
+
+<style>
+ .input{
+   @apply w-full py-2 border-b-2 border-brand outline-none  focus:bg-brand-50 ;
+ }  
+ textarea.input{
+   @apply bg-brand-50
+ }
+</style>
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,6 +1,16 @@
 <template>
-      <div class="flex flex-col justify-center items-center min-h-screen bg-gradient-to-b from-brand to-brand-900 text-brand-100">
-     ini halaman contact
+  <div class="grid grid-cols-1 md:grid-cols-2">
+    <div class=" flex flex-col justify-center items-start">
+      <div class="p-6 lg:p-10 ">
+        <div class="max-w-[600px] mx-auto">
+          <h4 class="text-4xl font-semibold">Contact Us</h4>
+          <p>Excellence in Hardware and Software Solutions</p>
+        </div>
+      </div>
+    </div>
+    <div class="p-6 lg:p-10 flex flex-col justify-center items-center bg-brand-50">
+      <FormContact class="max-w-[460px] w-full mx-auto bg-white rounded-lg border p-6 lg:p-10" />
+    </div>
   </div>
 </template>
 
@@ -8,6 +18,4 @@
 
 </script>
 
-<style>
-
-</style>
+<style></style>
